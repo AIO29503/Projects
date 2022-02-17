33facce84549eba7589b547f409c4ad28725d532
Aryan Agarwal <73215309+AIO29503@users.noreply.github.com>
2022-02-17 17:59:24 +0530

    Add files via upload

---
+++ Password Generator.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 17 16:12:52 2022
+
+@author: agarw
+"""
+
+import random as ran
+
+#Data:
+l = list("abcdefghijklmnopqrstuvwxyz")
+lc = list(("abcdefghijklmnopqrstuvwxyz").upper())
+sc = list("!@#$%^&*")
+num = list("1234567890")
+ml = l+sc+num+lc
+
+
+
+#Funtions:
+
+def randomiser(n):
+    pwd =[]
+    for i in range(n):
+        pwd.append(ran.choice(ml))
+    print("".join(pwd))
+
+def noncusran(req):
+    choicel = []
+    pwd =[]
+    for i in list(req):
+        if i == "c":
+            choicel = choicel + l
+        elif i == "n":
+            choicel = choicel + num
+        elif i == "s":
+            choicel = choicel + sc
+        elif i == "C":
+            choicel = choicel + lc
+    for i in range(n):    
+        pwd.append(ran.choice(choicel))
+    print("".join(pwd))
+
+def cusran(seq):
+    pwd =[]
+    for i in seq:
+        if i == "c":
+            pwd.append(ran.choice(l))
+        elif i == "n":
+            pwd.append(ran.choice(num))
+        elif i == "s":
+            pwd.append(ran.choice(sc))
+        elif i == "C":
+            pwd.append(ran.choice(lc))
+        else: 
+            pwd.append(i)
+    print("".join(pwd))
+    
+    
+#Main Program:    
+    
+n = int(input("Enter password length: "))
+rf = input("Do you want a random password?(y/n) ")
+    
+if rf == "y":
+    randomiser(n)
+else:
+    print("Enter c for letter, n for number, s for special character, C for capital letter, without space or if you want a totally custom password, enter 'custom'")
+    req = input("Enter Requirements: ")
+    if req != "custom":
+        noncusran(req)
+    else:
+        print("Enter password sequence (c for letter, n for number, s for special character, C for capital letter):\n ")
+        print("For example: if you want first 4 characters to be lowercase letters and last 4 to be numbers, type: ccccnnnn ")
+        while True:
+            seq = list(input(""))
+            if len(seq) != n:
+                print("Error: Enter sequence length as password length")
+            else: 
+                break
+        cusran(seq)
